64ace014515f86d4b2492dfed5246e18ac2a5add
Tahreem <tahreemnoor86@gmail.com>
2026-07-13 16:08:05 +0500

    auth middleware created

---
+++ server/middleware/auth.js
@@ -0,0 +1,33 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+const catchAsyncError = require('./catchAsyncError')
+const ErrorHandler = require("../utils/ErrorHandler");
+
+exports.isAuthenticated = catchAsyncError (async(req, res, next) => {
+    try {
+
+        const { token } = req.cookies;
+
+        if (!token) {
+            return next(new ErrorHandler("Please login first", 401));
+        }
+
+        const decoded = jwt.verify(
+            token,
+            process.env.JWT_SECRET_KEY
+        );
+
+        const user = await User.findById(decoded.id);
+
+        if (!user) {
+            return next(new ErrorHandler("User not found", 404));
+        }
+
+        req.user = user;
+
+        next();
+
+    } catch (error) {
+        next(new ErrorHandler(error.message, 401));
+    }
+});
